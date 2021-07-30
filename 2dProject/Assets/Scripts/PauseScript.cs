1cf6b389a9ad4e6640309d0b3af0de2a4c00ccd2
WiesY <mr.ryltsev@mail.ru>
2021-07-30 15:13:41 +0300

    reworked pause menu
    доделал фриз игры во время паузы
    сделал бэкграунд в mainmenu
    создал сцену с выбором миров

--- 2dProject/Assets/Scripts/PauseScript.cs
+++ 2dProject/Assets/Scripts/PauseScript.cs
@@ -8,7 +8,12 @@ public class PauseScript : MonoBehaviour
 
     public void PauseGame()
     {
+        Time.timeScale = 0f;
+    }
 
+    public void UnpauseGame()
+    {
+        Time.timeScale = 1f;
     }
 
     public void RestartGame()
