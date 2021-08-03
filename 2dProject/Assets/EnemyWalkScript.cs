1442942e7de862e808c20b3bc8b733f4e6317bd9
B-Oleg1 <73061913+B-Oleg1@users.noreply.github.com>
2021-08-03 19:59:08 +0300

    Scripts

--- 2dProject/Assets/EnemyWalkScript.cs
+++ 2dProject/Assets/EnemyWalkScript.cs
@@ -12,7 +12,7 @@ public class EnemyWalkScript : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D enemyRigidbody;
 
-    private float speed = 1.5f;
+    private float enemySpeed = 2.5f;
     private bool moveToLeftPoint = true;
 
 
@@ -23,82 +23,67 @@ public class EnemyWalkScript : MonoBehaviour
         enemyRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    //private void Update()
-    //{
-    //    if (targetPlayer != null)
-    //    {
-    //        pigIsAngry = true;
-    //    }
-    //    else
-    //    {
-    //        pigIsAngry = false;
-    //        PatrulZone();
-    //    }
-    //}
-
     private void FixedUpdate()
     {        
         if (targetPlayer != null)
         {
-            //TargetToPlayer();
+            TargetToPlayer();
         }
         else
         {
-            //PatrulZone();        
+            PatrulZone();        
         }
     }
 
-    //private void PatrulZone()
-    //{
-    //    if (transform.localPosition.x <= movePoints[0].transform.localPosition.x)
-    //    {
-    //        spriteRenderer.flipX = true;
-    //        moveToLeftPoint = false;
-    //    }
-    //    else if (transform.localPosition.x >= movePoints[1].transform.localPosition.x)
-    //    {
-    //        spriteRenderer.flipX = false;
-    //        moveToLeftPoint = true;
-    //    }
+    private void PatrulZone()
+    {
+        if (transform.localPosition.x <= movePoints[0].transform.localPosition.x)
+        {
+            spriteRenderer.flipX = true;
+            moveToLeftPoint = false;
+        }
+        else if (transform.localPosition.x >= movePoints[1].transform.localPosition.x)
+        {
+            spriteRenderer.flipX = false;
+            moveToLeftPoint = true;
+        }
 
-    //    if (transform.localPosition.x >= movePoints[0].transform.localPosition.x && moveToLeftPoint)
-    //    {            
-    //        enemyRigidbody.velocity = new Vector2(-3.5f, 0);
-    //    }
-    //    if (transform.localPosition.x <= movePoints[1].transform.localPosition.x && !moveToLeftPoint)
-    //    {            
-    //        enemyRigidbody.velocity = new Vector2(3.5f, 0);
-    //    }
-    //}
+        if (transform.localPosition.x >= movePoints[0].transform.localPosition.x && moveToLeftPoint)
+        {
+            enemyRigidbody.velocity = new Vector2(-enemySpeed, 0);
+        }
+        if (transform.localPosition.x <= movePoints[1].transform.localPosition.x && !moveToLeftPoint)
+        {
+            enemyRigidbody.velocity = new Vector2(enemySpeed, 0);
+        }
+    }
 
-    //private void TargetToPlayer()
-    //{        
-    //    if (transform.position.x > targetPlayer.transform.position.x)
-    //    {
-    //        spriteRenderer.flipX = false;
-    //        enemyRigidbody.velocity = new Vector2(-3.5f, 0);
-    //        moveToLeftPoint = true;
-    //    }
-    //    else if (transform.position.x < targetPlayer.transform.position.x)
-    //    {
-    //        spriteRenderer.flipX = true;
-    //        enemyRigidbody.velocity = new Vector2(3.5f, 0);
-    //        moveToLeftPoint = false;
-    //    }
-    //}
+    private void TargetToPlayer()
+    {
+        if (transform.position.x > targetPlayer.transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+            enemyRigidbody.velocity = new Vector2(-enemySpeed * 1.5f, 0);
+            moveToLeftPoint = true;
+        }
+        else if (transform.position.x < targetPlayer.transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+            enemyRigidbody.velocity = new Vector2(enemySpeed * 1.5f, 0);
+            moveToLeftPoint = false;
+        }
+    }
 
-    //public void SetTriggerPlayer(GameObject trigger)
-    //{
-    //    targetPlayer = trigger;
-    //    if (trigger != null)
-    //    {
-    //        animator.SetBool("Walk", false);
-    //        animator.SetBool("Run", true);
-    //    }
-    //    else
-    //    {
-    //        animator.SetBool("Run", false);
-    //        animator.SetBool("Walk", true);
-    //    }
-    //}
+    public void SetTriggerPlayer(GameObject trigger)
+    {
+        targetPlayer = trigger;
+        if (trigger != null)
+        {
+            animator.SetBool("Run", true);
+        }
+        else
+        {
+            animator.SetBool("Run", false);
+        }
+    }
 }
