46e19d6650f116fb3c5e11f98f824ae19f129a59
B-Oleg1 <73061913+B-Oleg1@users.noreply.github.com>
2021-08-03 14:29:03 +0300

    ai

--- 2dProject/Assets/EnemyWalkScript.cs
+++ 2dProject/Assets/EnemyWalkScript.cs
@@ -1,26 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyWalkScript : MonoBehaviour
 {
+    [SerializeField] private GameObject[] movePoints;
+
     public GameObject targetPlayer;
 
-    private float speed;
-    private GameObject[] movePoints;
     private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D enemyRigidbody;
+
+    private float speed = 1.5f;
+    private bool moveToLeftPoint = true;
+    private bool pigIsAngry = false;
+
 
     private void Awake()
     {
-        movePoints[0] = transform.parent.transform.Find("Left Point").gameObject;
-        movePoints[1] = transform.parent.transform.Find("Right Point").gameObject;
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        enemyRigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        if (Vector2.Distance(transform.position, movePoints[0].transform.position) == 0)
+        if (targetPlayer != null)
         {
-            animator.SetBool("Idle", true);
+            pigIsAngry = true;
+        }
+        else
+        {
+            pigIsAngry = false;
+            PatrulZone();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (pigIsAngry)
+        {
+            TargetToPlayer();
+        }
+        else
+        {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Run", false);
+            if (moveToLeftPoint)
+            {
+                enemyRigidbody.velocity = new Vector2(-3.5f, 0);
+            }
+            else
+            {
+                enemyRigidbody.velocity = new Vector2(3.5f, 0);
+            }            
+        }
+    }
+
+    private void PatrulZone()
+    {
+        if (transform.localPosition.x >= movePoints[1].transform.localPosition.x)
+        {
+            spriteRenderer.flipX = false;
+            moveToLeftPoint = true;
+        }
+        if (transform.localPosition.x <= movePoints[0].transform.localPosition.x)
+        {
+            spriteRenderer.flipX = true;
+            moveToLeftPoint = false;
+        }
+    }
+
+    private void TargetToPlayer()
+    {
+        animator.SetBool("Run", true);
+        animator.SetBool("Walk", false);
+        if (transform.position.x > targetPlayer.transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+            enemyRigidbody.velocity = new Vector2(-3.5f, 0);
+            moveToLeftPoint = true;
+        }
+        else if (transform.position.x < targetPlayer.transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+            enemyRigidbody.velocity = new Vector2(3.5f, 0);
+            moveToLeftPoint = false;
         }
     }
 }
