accaa9af987aba0ac9658dd2739f0e1790616ab0
B-Oleg1 <73061913+B-Oleg1@users.noreply.github.com>
2021-08-07 23:32:46 +0300

    Ghost is ready

--- 2dProject/Assets/GhostScript.cs
+++ 2dProject/Assets/GhostScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class GhostScript : MonoBehaviour
 {
-    public PointsTeleport[] pointsToTeleport;
+    [SerializeField] private PointsTeleport[] pointsToTeleport;
 
     private Animator ghostAnimator;
+    private AudioSource audioSource;
 
-    private float timeToTeleport = 10f;
+    private float timeToTeleport = 5f;
+    private bool canHit = true;
 
     private void Awake()
     {
         ghostAnimator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -22,19 +25,39 @@ public class GhostScript : MonoBehaviour
 
     private IEnumerator Teleport()
     {
-        int randomPointToTeleport = Random.Range(0, pointsToTeleport.GetLength(0));
+        while (true)
+        {
+            int randomPointToTeleport = Random.Range(0, pointsToTeleport.Length);
 
-        ghostAnimator.SetTrigger("StartTP");
-        yield return new WaitForSeconds(0.5f);
-        //transform.position = new Vector2(pointsToTeleport[randomPointToTeleport, 0].position.x, pointsToTeleport[randomPointToTeleport, 1].position.y);
-        ghostAnimator.SetTrigger("EndTP");
+            if (!ghostAnimator.GetBool("StartTP"))
+            {
+                ghostAnimator.SetBool("StartTP", true);
+            }
+            yield return new WaitForSeconds(1f);
+            transform.position = new Vector2(Random.Range(pointsToTeleport[randomPointToTeleport].point1.position.x, pointsToTeleport[randomPointToTeleport].point2.position.x), pointsToTeleport[randomPointToTeleport].point1.position.y);
+            ghostAnimator.SetTrigger("EndTP");
+            ghostAnimator.SetBool("StartTP", false);
+            canHit = true;
 
-        yield return new WaitForSeconds(timeToTeleport);
+            yield return new WaitForSeconds(timeToTeleport);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && canHit)
+        {
+            CharacterHealth.characterHealthInstance.EnemyHit();
+            audioSource.Play();
+            ghostAnimator.SetBool("StartTP", true);
+            canHit = false;
+        }
     }
 }
 
+[System.Serializable]
 public class PointsTeleport
 {
-    public Transform point1;
-    public Transform point2;
+    [SerializeField] protected internal Transform point1;
+    [SerializeField] protected internal Transform point2;
 }
