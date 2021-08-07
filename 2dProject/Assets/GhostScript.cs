36297e4338a4db49cd31fd915de7972e72b5efc0
B-Oleg1 <73061913+B-Oleg1@users.noreply.github.com>
2021-08-07 22:14:20 +0300

    s

--- 2dProject/Assets/GhostScript.cs
+++ 2dProject/Assets/GhostScript.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class GhostScript : MonoBehaviour
 {
-    [SerializeField] private Transform[,] points;
+    public PointsTeleport[] pointsToTeleport;
+
+    private Animator ghostAnimator;
+
+    private float timeToTeleport = 10f;
+
+    private void Awake()
+    {
+        ghostAnimator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Teleport());
+    }
+
+    private IEnumerator Teleport()
+    {
+        int randomPointToTeleport = Random.Range(0, pointsToTeleport.GetLength(0));
+
+        ghostAnimator.SetTrigger("StartTP");
+        yield return new WaitForSeconds(0.5f);
+        //transform.position = new Vector2(pointsToTeleport[randomPointToTeleport, 0].position.x, pointsToTeleport[randomPointToTeleport, 1].position.y);
+        ghostAnimator.SetTrigger("EndTP");
+
+        yield return new WaitForSeconds(timeToTeleport);
+    }
+}
+
+public class PointsTeleport
+{
+    public Transform point1;
+    public Transform point2;
 }
