e772f5d819e47cd530d8d6a9893fa54f32fd0f8e
B-Oleg1 <73061913+B-Oleg1@users.noreply.github.com>
2021-08-10 21:07:33 +0300

    Fixed

--- 2dProject/Assets/BlockScript.cs
+++ 2dProject/Assets/BlockScript.cs
@@ -10,19 +10,19 @@ public class BlockScript : MonoBehaviour
     private void Start()
     {
         blockRigidbody = GetComponent<Rigidbody2D>();
+        destoyedObjects[0].SetActive(false);
+        destoyedObjects[1].SetActive(false);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (blockRigidbody.velocity.x > 5f || blockRigidbody.velocity.x < -1.95f)
-        {
-            Instantiate(destoyedObjects[0], new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
-            Instantiate(destoyedObjects[1], new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        {            
+            destoyedObjects[0].transform.position = new Vector2(transform.position.x, transform.position.y + 0.15f);
+            destoyedObjects[1].transform.position = new Vector2(transform.position.x, transform.position.y - 0.15f);
             gameObject.SetActive(false);
-            //destoyedObjects[0].transform.position = new Vector2(transform.position.x, transform.position.y + 0.15f);
-            //destoyedObjects[1].transform.position = new Vector2(transform.position.x, transform.position.y - 0.15f);
-            //destoyedObjects[0].SetActive(true);
-            //destoyedObjects[1].SetActive(true);
+            destoyedObjects[0].SetActive(true);
+            destoyedObjects[1].SetActive(true);
             destoyedObjects[0].GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)) * blockRigidbody.velocity.x * 10);
             destoyedObjects[1].GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f)) * blockRigidbody.velocity.x * 10);
         }
