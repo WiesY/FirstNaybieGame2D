09a473c069a44a50d863a8d7d76d2a61c6d24f16
B-Oleg1 <73061913+B-Oleg1@users.noreply.github.com>
2021-08-20 19:43:33 +0300

    Rework lvl 3

--- 2dProject/Assets/Scripts/CharacterController/PlayerController.cs
+++ 2dProject/Assets/Scripts/CharacterController/PlayerController.cs
@@ -33,17 +33,17 @@ public class PlayerController : MonoBehaviour
     //    joystick = transform.parent.GetComponent<SetCharacterScript>().joystick;
     //}
 
-    //private void Update()
-    //{
-    //    if (Input.GetKey(KeyCode.W))
-    //    {
-    //        Jump();
-    //    }
-    //}
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Jump();
+        }
+    }
 
     private void FixedUpdate()
     {
-        // movement = Input.GetAxis("Horizontal") * playerSpeed; // Keyboard move(desktop)
+        movement = Input.GetAxis("Horizontal") * playerSpeed; // Keyboard move(desktop)
         //if (joystick.Horizontal > 0.1f) // Joystick move(telephone)
         //{
         //    movement = playerSpeed;
@@ -57,18 +57,18 @@ public class PlayerController : MonoBehaviour
         //    movement = 0;
         //}
 
-        if (leftMove) // Buttons move(telephone)
-        {
-            movement = -playerSpeed;
-        }
-        else if (rightMove)
-        {
-            movement = playerSpeed;
-        }
-        else
-        {
-            movement = 0;
-        }
+        //if (leftMove) // Buttons move(telephone)
+        //{
+        //    movement = -playerSpeed;
+        //}
+        //else if (rightMove)
+        //{
+        //    movement = playerSpeed;
+        //}
+        //else
+        //{
+        //    movement = 0;
+        //}
 
         playerRigidbody.velocity = new Vector2(movement, playerRigidbody.velocity.y);
 
